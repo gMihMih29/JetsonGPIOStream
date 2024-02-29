ecc5fbec2dd04ef5ee1510ab3166e82d0c53294e
Mikhail Getmanov <60793839+gMihMih29@users.noreply.github.com>
2024-02-29 11:06:15 +0300

    Revert "Feature/work with files"

--- src/GPIO_in/GPIOStreamIn.h
+++
@@ -1,9 +0,0 @@
-namespace JetsonGPIOStream {
-class GPIOStreamIn {
-public:
-    void SayHello();
-
-private:
-};
-
-}  // namespace JetsonGPIOStream
--- src/IOFileBin/WriteDataToFile.h
+++
@@ -1,6 +0,0 @@
-#include <fstream>
-#include <stdexcept>
-#include <string>
-#include <vector>
-
-void WriteDataToFile(const std::string& outputPath, const std::vector<unsigned char>& data);
